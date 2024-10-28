0c86f940f937c5d6b9097a74a1772d405567ade5
hatrinh123 <haphuongtrinh0306@gmail.com>
2024-10-29 00:05:05 +0700

    BTVNBuoi2

--- BTVNBuoi2/BaiTap1.java
+++ BTVNBuoi2/BaiTap1.java
@@ -1,7 +1,44 @@
 package BTVNBuoi2;
 
+import java.util.Scanner;
+
+import java.util.Arrays;
+
 public class BaiTap1 {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    public static int search(int [] a, int n, int k){
+        int index=-1;
+        int l=0, r=n-1;
+        while(l<=r){
+            int m=(l+r)/2;
+            if(a[m]==k){
+                index=m;
+                r=m-1;
+            }
+            else if(a[m]>k){
+                r=m-1;
+            }
+            else{
+                l=m+1;
+            }
+        }
+        return index;
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n,k;
+        n=sc.nextInt();
+        k=sc.nextInt();
+        int[] a = new int[n];
+        for (int i=0; i<n; i++){
+            a[i]=sc.nextInt();
+        }
+        Arrays.sort(a);
+        for (int b : a) {
+            System.out.print(b + " ");
+        }
+        System.out.println();
+        System.out.println(search(a,n,k));
+
     }
 }
+
